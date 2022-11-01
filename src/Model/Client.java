f131288a421c871e208ca32eae4f3c679effd5b4
Nizar <seffarnizar.pro@gmail.com>
2022-11-02 00:46:46 +0100

    Wrote code of Client and Compte and did some tests

--- src/Model/Client.java
+++ src/Model/Client.java
@@ -1,4 +1,73 @@
 package Model;
 
+import java.util.Scanner;
+
 public class Client {
+    private static int ctr=0;
+    private int idclient;
+    private String nom;
+    private String prenom;
+    private Compte[] comptes = new Compte[2];
+
+    public void setNom(String nom) {
+        if(nom.matches("[A-Za-z ]*")) {
+            this.nom = nom;
+        }
+        else{
+            Scanner clavier = new Scanner(System.in);
+            System.out.println("Attention!!Veuillez saisir un nom Valide:");
+            nom = clavier.nextLine();
+            setNom(nom);
+
+
+        }
+
+    }
+
+    public void setPrenom(String prenom) {
+        if(prenom.matches("[A-Za-z ]*")) {
+            this.prenom = prenom;
+        }
+        else{
+            Scanner clavier = new Scanner(System.in);
+            System.out.println("Attention!!Veuillez saisir un prenom Valide:");
+            prenom = clavier.nextLine();
+            setPrenom(prenom);
+        }
+
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public int getIdclient() {
+        return idclient;
+    }
+    Client(){
+        idclient = ++ctr;
+    }
+    Client(String nom , String prenom){
+        idclient = ++ctr;
+        setNom(nom);
+        setPrenom(prenom);
+    }
+
+   /* public static void main(String[] args) {
+        Client c1 = new Client();
+        String nom , prenom;
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Veuillez saisir le nom du client");
+        nom = clavier.nextLine();
+        c1.setNom(nom);
+        System.out.println("Veuillez saisir le prenom du client");
+        prenom= clavier.nextLine();
+        c1.setPrenom(prenom);
+        System.out.println("Le nom du client est : "+ c1.getNom() +" et son prénom est: "+c1.getPrenom()+" et son id est:"+c1.getIdclient());
+    } */
+
 }
--- src/Model/Compte.java
+++ src/Model/Compte.java
@@ -1,4 +1,111 @@
 package Model;
+import java.util.Date;
+import java.time.LocalDateTime;
+import  java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class Compte {
+    private  static  int ctr=0;
+    private    int ctr2=0;
+    private  int idcompte;
+    private  double solde;
+    private  String[] journalisation = new String[10];
+    private  String date_creation;
+    private  Client proprio;
+
+
+
+    public void setSolde(double solde) {
+        if(solde>=0){
+        this.solde = solde;
+        }
+        else {
+            Scanner clavier = new Scanner(System.in);
+            System.out.println("Attention!!Veuillez saisir un solde positife:");
+            solde = clavier.nextDouble();
+            setSolde(solde);
+        }
+        }
+
+
+    public void setDate_creation(String date_creation) {
+        this.date_creation = date_creation;
+    }
+
+    public void setProprio(Client proprio) {
+        this.proprio = proprio;
+    }
+
+    public void setJournalisation() {
+        this.journalisation[ctr] = this.getDate_creation();
+    }
+
+    public Client getProprio() {
+        return proprio;
+    }
+
+    public double getSolde() {
+
+        return solde;
+    }
+
+    public String[] getJournalisation() {
+        return journalisation;
+
+
+    }
+
+    public int getIdcompte() {
+        return idcompte;
+    }
+
+    public String getDate_creation() {
+        return date_creation;
+    }
+
+
+     Compte(){
+        DateTimeFormatter dtf= DateTimeFormatter.ofPattern("dd/MM/YYYY hh:mm:ss");
+         String current_date=dtf.format(LocalDateTime.now());
+         idcompte = ++ctr;
+        setDate_creation(current_date);
+         journalisation[ctr2] = current_date;
+         ++ctr2;
+         setSolde(0.0);
+
+     }
+     Compte(Client c ,double solde){
+         DateTimeFormatter dtf= DateTimeFormatter.ofPattern("dd/MM/YYYY hh:mm:ss");
+         String current_date=dtf.format(LocalDateTime.now());
+         idcompte = ++ctr;
+         setDate_creation(current_date);
+         journalisation[ctr2] = current_date;
+         ++ctr2;
+         setSolde(solde);
+         setProprio(c);
+     }
+
+
+    public static void main(String[] args) {
+        Client c = new Client("Nom","Prenom");
+        Compte c1 = new Compte();
+        double solde;
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Veuillez Saisir un solde:");
+        solde = clavier.nextDouble();
+        c1.setProprio(c);
+        c1.setSolde(solde);
+        System.out.println("Solde: "+c1.getSolde() +" et: "+Arrays.toString(c1.journalisation));
+
+
+
+
+
+
+
+
+
+    }
 }
